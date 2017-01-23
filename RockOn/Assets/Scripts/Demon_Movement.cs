ee6ccaa172b3fac4d786ea24db1ef19aaf0ffcc0
Mateusz Jabłoński <mateusz.jablonski.94@gmail.com>
2017-01-23 17:43:38 +0100

    Demons move when player sees them

--- RockOn/Assets/Scripts/Demon_Movement.cs
+++ RockOn/Assets/Scripts/Demon_Movement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Demon_Movement : MonoBehaviour
 {
     private float _speed; // movement speed
-    private float _maxRange; // max range at which it stops chasing the target (it's too far)
+    //private float _maxRange; // max range at which it stops chasing the target (it's too far) // no longer used
     private float _minRange; // min range at which it stops chasing the target (it's too close)
 
     private Transform _target; // target's position
@@ -14,7 +14,7 @@ public class Demon_Movement : MonoBehaviour
     private Demon_Attack_Range _attackScript; // for changing speed when attacking
     private float _distance; // distance between enemy and target
     private float _pushBackPower; // how strong is enemy pushed back when pick is active
-
+    private bool _isInRange; // is enemy in range of the player's view?
     private Enemy_Audio _ea;  // C'mon! sounds
 
     void Start()
@@ -27,12 +27,11 @@ public class Demon_Movement : MonoBehaviour
         _attackScript = GetComponentInChildren<Demon_Attack_Range>();
         _ea = GetComponent<Enemy_Audio>();
 
-        
         _speed = 1.5f;
-        _maxRange = 3.0f;
+        //_maxRange = 3.0f; // no longer used
         _minRange = 0.67f;
         _pushBackPower = 30.0f;
-
+        _isInRange = false;
     }
 
     // called when enemy is attacked and pick power-up is active
@@ -49,12 +48,12 @@ public class Demon_Movement : MonoBehaviour
         _distance = Vector2.Distance(_enemy.position, _target.position);
 
         // chase target only within set range
-        if (_distance <= _maxRange && _distance >= _minRange)
+        if (_isInRange && _distance >= _minRange) // && _distance <= _maxRange
         {
             // moving the object, animate
             _anim.SetBool("isMoving", true);
 
-            StartCoroutine(Wait(_ea,2)); //play C'mon! sound
+            StartCoroutine(Wait(_ea, 2)); //play C'mon! sound
 
             // calculate direction (and normalize it so it doesn't change the speed of movement)
             Vector2 direction = new Vector2(_target.position.x - _enemy.position.x, _target.position.y - _enemy.position.y).normalized;
@@ -83,5 +82,10 @@ public class Demon_Movement : MonoBehaviour
         _ea.enabled = true;
         yield return new WaitForSeconds(delay);
         _ea.enabled = false;
+    }
+
+    public void setIsInRange(bool isInRange)
+    {
+        _isInRange = isInRange;
     }
 }
---
+++ RockOn/Assets/Scripts/ViewRange_Trigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewRange_Trigger : MonoBehaviour
+{
+    // event that is called if target enters this Object's collider (is in range)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Demon")
+        {
+            collision.gameObject.GetComponent<Demon_Movement>().setIsInRange(true);
+        }
+    }
+
+    // event that is called if target exits this Object's collider (is out of range)
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Demon")
+        {
+            collision.gameObject.GetComponent<Demon_Movement>().setIsInRange(false);
+        }
+    }
+}
