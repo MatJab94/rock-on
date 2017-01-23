56f6f6ee1a72ef9e2670195704db83101dd8d692
Igor <igor.filipiuk@gmail.com>
2017-01-24 00:05:18 +0100

    changed metronome sound and added MainMenu bg music

---
+++ RockOn/Assets/MainMenuBgMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBgMusic : MonoBehaviour {
+
+    // Use this for initialization
+
+    public AudioSource bgAudio;
+
+	void Start () {
+
+        AudioListener.pause = false;
+        Time.timeScale = 1;
+        bgAudio.Play();
+	}
+	
+}
