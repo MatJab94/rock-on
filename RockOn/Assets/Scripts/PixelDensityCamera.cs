ba5ff303d2e89efa006639e2797c02125314fed7
Mateusz Jabłoński <mateusz.jablonski.94@gmail.com>
2017-05-13 23:00:32 +0200

    Aspect Ratio, resolutions, black bars, small fixes
    aspect ratio is now always 4:3 with black bars if resolution is wider,
    added support for all resolutions/aspect ratios, because visible area is
    always the same now, changed position of camera

---
+++ RockOn/Assets/Scripts/PixelDensityCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelDensityCamera : MonoBehaviour
+{
+
+    public float pixelsToUnits = 100;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+
+        cam.orthographicSize = Screen.height / pixelsToUnits / 2;
+    }
+}
---
+++ RockOn/Assets/Scripts/ScaleWidthCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScaleWidthCamera : MonoBehaviour
+{
+
+    public int targetWidth = 640;
+    public float pixelsToUnits = 100;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+
+        int height = Mathf.RoundToInt(targetWidth / (float)Screen.width * Screen.height);
+
+        cam.orthographicSize = height / pixelsToUnits / 2;
+    }
+}
