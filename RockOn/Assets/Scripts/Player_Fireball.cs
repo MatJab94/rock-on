5d23d53fa849034a34cbc65c69eae3fadaf5829a
Mateusz Jabłoński <mateusz.jablonski.94@gmail.com>
2017-05-28 00:06:50 +0200

    test_newAttack
    player attacks with fireballs now, it's still buggy but whatevs

---
+++ RockOn/Assets/Scripts/Player_Fireball.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Fireball : MonoBehaviour
+{
+    private float _moveSpeed; // movement speed
+    private float _rotateSpeed; // rotation speed
+    private Vector3 _target; // target's position
+    private Transform _fireball; // this object's position
+    private Rigidbody2D _rb; // this objects's rigidbody2d
+    private float _distance; // distance between enemy and target
+    private Animator _anim; // animator of this object
+
+    void Start()
+    {
+        // initializing variables
+        Transform Cursor = GameObject.FindGameObjectWithTag("Cursor").GetComponent<Transform>();
+        _target = new Vector3(Cursor.position.x, Cursor.position.y, Cursor.position.z);
+
+        _fireball = GetComponent<Transform>();
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+
+        _moveSpeed = 3.0f;
+        _rotateSpeed = 15.0f;
+
+        // rotate fireball towards target on spawn
+        _fireball.rotation = getRotation();
+        rotateFireball();
+    }
+
+    void FixedUpdate()
+    {
+        moveFireball();
+    }
+
+    void rotateFireball()
+    {
+        // some magic code from the internets, rotates the fireball towards target
+        Quaternion q = getRotation();
+        _fireball.rotation = Quaternion.RotateTowards(transform.rotation, q, _rotateSpeed);
+    }
+
+    void moveFireball()
+    {
+        // move fireball forward ("forward" direction is based on rotation)
+        _rb.AddForce(_fireball.right * _moveSpeed, ForceMode2D.Impulse);
+    }
+
+    Quaternion getRotation()
+    {
+        Vector3 vectorToTarget = _target - _fireball.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+        return Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    // objects need to subscribe and unsubscribe from events when they're enabled/disabled
+    private void OnEnable()
+    {
+        RythmBattle.OnGoodRythm += rythmAnimation;
+    }
+    private void OnDisable()
+    {
+        RythmBattle.OnGoodRythm -= rythmAnimation;
+    }
+
+    // animate player on rythm events
+    void rythmAnimation()
+    {
+        _anim.SetTrigger("beat");
+    }
+}
---
+++ RockOn/Assets/Scripts/Player_Fireball_Attack.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Fireball_Attack : MonoBehaviour
+{
+    // is microphpne power-up active?
+    private bool _micActive;
+
+    // max range at which target can be interacted with
+    public float defaultMaxRange;
+    private float _maxRange;
+    private float maxRangeWithMicrophone;
+
+    // damage of the regular attack
+    private int _currentDamage;
+    private int _regularDamage;
+    private int _damageWithMicrophone;
+
+    // script that stops player from continuously attacking
+    private Player_AttackTimeOut _timeoutScript;
+
+    // for disabling the attacking on first level
+    public bool attackDisabled;
+
+    // to get value of rythm flag
+    private RythmBattle rythmBattleScript;
+
+    // player's audio script for making sounds when attacking
+    private Player_Audio _playerAudio;
+    
+    // prafab of player's fireball, set in Inspector
+    public GameObject fireballPrefab;
+
+    void Start()
+    {
+        _micActive = false;
+
+        _maxRange = defaultMaxRange;
+        maxRangeWithMicrophone = defaultMaxRange * 1.5f;
+
+        _regularDamage = 1;
+        _damageWithMicrophone = 2;
+        _currentDamage = _regularDamage;
+
+        _timeoutScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player_AttackTimeOut>();
+
+        attackDisabled = false;
+
+        rythmBattleScript = GameObject.FindGameObjectWithTag("RythmBattle").GetComponent<RythmBattle>();
+
+        _playerAudio = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Player_Audio>();
+    }
+
+    void Update()
+    {
+        if (_micActive)
+        {
+            _maxRange = maxRangeWithMicrophone;
+            _currentDamage = _damageWithMicrophone;
+        }
+        else
+        {
+            _maxRange = defaultMaxRange;
+            _currentDamage = _regularDamage;
+        }
+
+        // if player clicks the attack button (mouse 0 by default)
+        if (Input.GetButtonDown("Regular_Attack") && _timeoutScript.getTimeoutFlag() == false && !attackDisabled)
+        {
+            //getting rythm flag for bonuses to mana if attacking in rythm
+            regularAttack(rythmBattleScript.getRythmFlag());
+
+            // start timeout after attacking
+            _timeoutScript.startTimeout();
+        }
+    }
+
+    private void regularAttack(bool rythmFlag)
+    {
+        // play attack's sound
+        _playerAudio.playChordSound();
+
+        //shoot the fireball towards cursor's direction
+        GameObject fireball = Instantiate(fireballPrefab, gameObject.transform.position, Quaternion.identity);
+        fireball.gameObject.GetComponentInChildren<PlayerFireball_Attack>().setDamageAndRythm(_currentDamage, rythmFlag);
+    }
+}
