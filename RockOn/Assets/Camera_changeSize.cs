3b4fe90b9f0acdac518316a2d6b3d15692025ced
Mateusz Jabłoński <mateusz.jablonski.94@gmail.com>
2017-07-09 21:01:41 +0200

    Level 3 overview, camera settings
    During presentation we can go to level 3 and show it off, we can change
    camera viewing distance with O and P

---
+++ RockOn/Assets/Camera_changeSize.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_changeSize : MonoBehaviour
+{
+    Camera cam;
+	void Start()
+	{
+        cam = gameObject.GetComponent<Camera>();
+	}
+
+	void Update()
+	{
+        if (Input.GetKey(KeyCode.P))
+        {
+            cam.orthographicSize += 0.5f;
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            cam.orthographicSize -= 0.5f;
+        }
+    }
+}
